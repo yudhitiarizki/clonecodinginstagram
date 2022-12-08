b7a04b294d8d4efbbea1f3e299f01fb21a7a850c
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-12-08 12:24:07 +0700

    edit route

--- front-end/src/shared/Router.js
+++ front-end/src/shared/Router.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreatePost from "../components/create post/CreatePost";
+import Explore from "../components/explore/Explore";
+import DirectMessages from "../components/messages/DirectMessages";
+import More from "../components/more/More";
+import Notifs from "../components/notifs/Notifs";
+import Profile from "../components/profile/Profile";
+import Search from "../components/search/Search";
 import Homepage from "../pages/Homepage";
 import Loginpage from "../pages/Loginpage";
 
@@ -9,6 +16,13 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Loginpage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/messages" element={<DirectMessages />} />
+        <Route path="/notifs" element={<Notifs />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/more" element={<More />} />
       </Routes>
     </BrowserRouter>
   );
